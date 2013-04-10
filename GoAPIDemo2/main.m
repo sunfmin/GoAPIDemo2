db3d5ca893fb1f9f600879c99e54ebab11665c22
Felix Sun <sunfmin@gmail.com>
2013-04-10 16:51:23 +0800

    Initial Commit

---
+++ GoAPIDemo2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GoAPIDemo2
+//
+//  Created by Felix Sun on 4/10/13.
+//  Copyright (c) 2013 Felix Sun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
